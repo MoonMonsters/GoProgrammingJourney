db0c1e2e412b92f03f78fd60a8ab2fbcbc2034f4
chentao <chentao@jiguang.cn>
2020-12-08 21:35:11 +0800

    token中间件

---
+++ blog_service/internal/middleware/jwt.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"GoProgrammingJourney/blog_service/pkg/app"
+	"GoProgrammingJourney/blog_service/pkg/errcode"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var (
+			token string
+			ecode = errcode.Success
+		)
+
+		if s, exists := c.GetQuery("token"); exists {
+			token = s
+		} else {
+			token = c.GetHeader("token")
+		}
+
+		if token == "" {
+			ecode = errcode.InvalidParams
+		} else {
+			_, err := app.ParseToken(token)
+			if err != nil {
+				switch err.(*jwt.ValidationError).Errors {
+				case jwt.ValidationErrorExpired:
+					ecode = errcode.UnauthorizedTokenTimeout
+				default:
+					ecode = errcode.UnauthorizedTokenError
+				}
+			}
+		}
+
+		if ecode != errcode.Success {
+			response := app.NewResponse(c)
+			response.ToErrorResponse(ecode)
+			c.Abort()
+
+			return
+		}
+
+		c.Next()
+	}
+}
--- blog_service/internal/routers/router.go
+++ blog_service/internal/routers/router.go
@@ -30,6 +30,7 @@ func NewRouter() *gin.Engine {
 	r.GET("/auth", v1.GetAuth)
 
 	apiV1 := r.Group("/api/v1")
+	apiV1.Use(middleware.JWT())
 	{
 		apiV1.POST("tags", tag.Create)
 		apiV1.DELETE("/tags/:id", tag.Delete)
@@ -43,6 +44,7 @@ func NewRouter() *gin.Engine {
 		apiV1.PATCH("/articles/:id/state", article.Update)
 		apiV1.GET("/articles/:id", article.Get)
 		apiV1.GET("/articles", article.List)
+
 	}
 
 	return r
